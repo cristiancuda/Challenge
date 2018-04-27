c6782c2f6aeffbedb3f40b716cff1e8a36037ac9
Cristian Cuda <cudacristian@gmail.com>
2018-04-27 11:50:09 -0700

    Update parse.php

--- parse.php
+++ parse.php
@@ -1,47 +1,24 @@
-<html>
-<head>
-  <title>Parse</title>
-</head>
-<body>
-
 <?php
+  header("Content-type: application/json;");
   require "simple_html_dom.php";
-  $endpoint = $_GET["endpoint"]; //Gets endpoint parameter, and assigns it to $endpoint.
+  $endpoint = $_GET["endpoint"];
   $tag = $_GET["tag"];
-  $html = file_get_html('http://'.$endpoint); // Creating DOM from html file passed by url.
+  $html = file_get_html('http://'.$endpoint);
   //Variable declarations.
   $arrayItem = array();
-  $arrayItem2 = array();
   $count = 0;
-  $count2 = 0;
-  //Looking into the DOM for (all) the tags that we obtained through $_GET["tag"], and storing those into an array.
   foreach($html->find('//'.$tag) as $value){
-	  $arrayItem[$count] = $value->innertext;
-	  $count++;
+    $arrayItem[$count] = $value->innertext;
+    $count++;
   }
-  //Printing the array (Testing purposes)
-  echo 'Array:  ';
-  echo json_encode($arrayItem);
-  //Printing with the required format.
-  echo '<pre>';
   echo '{';
-  echo '<br>';
   echo '  ' . $tag . ': [';
-  echo '<br>';
   foreach($arrayItem as $value){
-	  echo '   {';
-	  echo '<br>';
-	  echo '    innerText: ' . $value;
-	  echo '<br>';
-	  echo '    innerHtml: ' . json_encode($value);
-	  echo '<br>';
-	  echo '   },';
-	  echo '<br>';
+    echo '   {';
+    echo '    innerText: ' . $value;
+    echo '    innerHtml: ' . json_encode($value);
+    echo '   },';
   }
-  echo '  ' . ']';
-  echo '<br>';
-  echo '}';
-  echo '</pre>';
+echo '  ' . ']';
+echo '}';
 ?>
-</body>
-</html>
